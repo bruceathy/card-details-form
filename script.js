c10ff58ecf5ef060ab4e870471f58b0a44212ced
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-09-24 10:24:40 -0400

    functionality start

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 const cardForm = document.getElementById("card-form");
 const submitBtn = document.getElementById("submit");
 const thankYou = document.getElementById("thank-you");
-const cardNumber = document.getElementById("number");
-const cardName = document.getElementById("card-name");
-const cardExp = document.getElementById("card-exp");
+const cardNumber = document.getElementById("input-number");
+const cardNumberError = document.getElementById("number-error");
+const cardName = document.getElementById("input-name");
+const cardNameError = document.getElementById("name-error");
+const expireMonth = document.getElementById("exp-month");
+const expireMonthError = document.getElementById("date-error");
+const expireYear = document.getElementById("exp-year");
+const expireYearError = document.getElementById("date-error");
 const continueBtn = document.getElementById("continue");
+
+function cardNameValidation() {
+  if (cardName.value === "") {
+    cardNameError.classList.remove("hidden");
+  } else {
+    return true;
+  }
+}
+
+function cardNumberValidation() {
+  if (cardNumber.value === "") {
+    cardNumberError.classList.remove("hidden");
+  } else if (cardNumber.value.length < 16 || cardNumber.value.length > 16) {
+    cardNumberError.classList.remove("hidden");
+  } else {
+    return;
+  }
+}
+
+function expireMonthValidation() {
+  if (expireMonth.value === "") {
+    expireMonthError.classList.remove("hidden");
+  } else {
+    return true;
+  }
+}
+
+function expireYearValidation() {
+  if (expireYear.value === "") {
+    expireYearError.classList.remove("hidden");
+  } else {
+    return true;
+  }
+}
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  // cardNameValidation();
+  // cardNumberValidation();
+  // expireMonthValidation();
+  // expireYearValidation();
   cardForm.classList.add("hidden");
   thankYou.classList.remove("hidden");
 });
